0f8b8a8ed1755e1ba5638cd54469044d290ca4a6
Thanh Dat Do <dthanh421@gmail.com>
2024-09-11 08:52:43 +0700

    Implement error handlers

--- src/app.js
+++ src/app.js
@@ -2,6 +2,12 @@ const express = require("express");
 const cors = require("cors");
 const JSend = require("./jsend");
 const contactsRouter = require("./routes/contacts.router");
+
+const {
+    resourceNotFound,
+    handleError,
+} = require("./controllers/errors.controller");
+
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -10,4 +16,9 @@ app.get("/", (req, res) => {
     return res.json(JSend.success());
 });
 contactsRouter.setup(app);
+
+// Handle 404 response
+app.use(resourceNotFound);
+
+app.use(handleError);
 module.exports = app;
